68f27c0030a630e2309dbeed6ef71f81367e8ae5
twixx sturgill <twixxcustoms@gmail.com>
2019-08-26 15:18:26 -0400

    under construction

---
+++ main.js
@@ -0,0 +1,4 @@
+var typed = new Typed('.element', {
+    strings: ["Hi, my name is Xandric Hesson, and I am a fullstack JavaScript developer. ^1000", "I spend most of my time doing photography, writing, playing with my dogs and toying with code. ^1000", "I attended Eleven Fifty Academy, which taught me a discipline for coding under time constraints and learning new things as well as being able to solve problems efficiently"],
+    typeSpeed: 30
+  });
